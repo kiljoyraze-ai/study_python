85071af2dc0e14e30196ae241235a6cb6898aa62
kiljoyraze-ai <brian@gmail.com>
2025-10-05 06:26:53 +0700

    pelatihan pagi ini 06:26

--- baru/fungsi.py
+++ baru/fungsi.py
@@ -12,3 +12,20 @@ def indentity(nama , nik, nis, agama):
      "nis  :|"+     str(nis)  + "|\n"
      "agama:|"+     agama     + "|\n")
 indentity("Rudo", 121300, 123222, "Catholic")
+
+#fungsi sebuah kalkulator
+def kalkulator():
+    angka = int(input("masukkan angka pertama: "))
+    angka2 = int(input("masukkan angka kedua  : "))
+   
+    print("=====CALCULATOR=====\n"
+        "num1: ", angka,  "\n"
+        "num2: ", angka2 )
+    
+    penjumlahan = angka + angka2
+    pengurangan = angka - angka2
+    pembagian = angka / angka2
+    perkalian = angka * angka2
+    print("hasil penjumlahan dari ",angka, "+", angka2 ,"=", penjumlahan)
+
+kalkulator()
