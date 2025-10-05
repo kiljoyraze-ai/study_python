f68ed7291ee97e1b1168e56a767d24107771df20
kiljoyraze-ai <brian@gmail.com>
2025-10-05 08:12:31 +0700

    updated: mengupdate sisitem fungsi kalkulator

--- baru/fungsi.py
+++ baru/fungsi.py
@@ -4,29 +4,47 @@ def hola(nama):
 
 hola("DoniAjif")
 
-def indentity(nama , nik, nis, agama):
-     
-    print("======IDENTITAS======\n"
-     "nama :|"+     nama      + "|\n"
-     "nik  :|"+     str(nik)  + "|\n"
-     "nis  :|"+     str(nis)  + "|\n"
-     "agama:|"+     agama     + "|\n")
-indentity("Rudo", 121300, 123222, "Catholic")
+def indentity():
+     nama = str(input(" Nama: "))
+     nik = int(input("  Nik:"))
+     nis = int(input("  Nis: "))
+     agama = str(input("Agama: "))
+
+     print("====IDENTITAS-DIRI==== \n"
+          "Nama : ", nama, "\n" 
+          "Nik  : ", nik, "\n"
+          "Nis  : ", nis, "\n"
+          "Agama: ", agama)
+indentity()
+
+# indentity("Rudo", 121300, 123222, "Catholic")
 
 #fungsi sebuah kalkulator
 def kalkulator():
+    pickOperator = input("Pilih oprasi (+ , - , * , /): ")
     angka = int(input("masukkan angka pertama: "))
     angka2 = int(input("masukkan angka kedua  : "))
-   
+    hasil = ''
+    operator = ""
+    match pickOperator:
+        case "+":
+            operator = "Penjumlahan"
+            hasil = angka + angka2
+        case "-":
+            operator = "Pengurangan"
+            hasil = angka - angka2
+        case "*":
+            operator = "Perkalian"
+            hasil = angka * angka2
+        case "/":
+            operator = "Pembagian"
+            hasil = angka / angka2
+
     print("=====CALCULATOR=====\n"
         "num1: ", angka,  "\n"
-        "num2: ", angka2 )
+        "num2: ", angka2, "\n",
+        "Hasil dari {}".format(operator) , hasil)
     
-    penjumlahan = angka + angka2
-    pengurangan = angka - angka2
-    pembagian = angka / angka2
-    perkalian = angka * angka2
-    print("hasil penjumlahan dari ",angka, "+", angka2 ,"=", penjumlahan)
 
 kalkulator()
 #kesalahan pertama dalam menggunakan fungsi 
